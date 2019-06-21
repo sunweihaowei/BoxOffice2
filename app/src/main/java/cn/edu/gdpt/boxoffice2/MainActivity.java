dd4d0d4ff205212d4a471a2440b14d1902be2407
sunweihao <1096187594@qq.com>
2019-06-21 23:05:18 +0800

    pieChartView

--- app/src/main/java/cn/edu/gdpt/boxoffice2/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/boxoffice2/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.edu.gdpt.boxoffice2;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.design.widget.TabLayout;
@@ -24,6 +25,10 @@ import java.util.Map;
 import cn.edu.gdpt.boxoffice2.Adapter.MyAdapter;
 import cn.edu.gdpt.boxoffice2.Adapter.pagerAdapter;
 import cn.edu.gdpt.boxoffice2.bean.main;
+import lecho.lib.hellocharts.listener.PieChartOnValueSelectListener;
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -42,6 +47,25 @@ public class MainActivity extends AppCompatActivity {
     private List<View> viewList=new ArrayList<>();
     private List<String> nameList=new ArrayList<>();
     private String[] names=new String[]{"饼状图","条形图","线形图"};
+    private PieChartView pcv;
+    private View view,view1,view2;
+
+
+    private int[] pieData=new int[]{
+            10,20,30,35,5
+    };
+    private int[] color=new int[]{
+            Color.parseColor( "#356fb3" ),
+            Color.parseColor( "#b53633" ),
+            Color.parseColor( "#86aa3d" ),
+            Color.parseColor( "#6a4b90" ),
+            Color.parseColor( "#2e9cba" )
+    };
+    List<SliceValue> sliceValues=new ArrayList <>(  );
+    private String[] stateChar={"高等教育1","高等教育2","高等教育3","高等教育4","高等教育5"};
+    private PieChartData data;
+
+
 
     /*private PullToRefreshView pulltorefreshView;*/
     @Override
@@ -50,12 +74,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
 
-        View view=View.inflate(MainActivity.this,R.layout.pie_item,null);
+        view=View.inflate(MainActivity.this,R.layout.pie_item,null);
         viewList.add(view);
-        View view1=View.inflate(MainActivity.this,R.layout.hc_line,null);
+        view1=View.inflate(MainActivity.this,R.layout.hc_line,null);
         viewList.add(view1);
-        View view2=View.inflate(MainActivity.this,R.layout.hc_c,null);
+        view2=View.inflate(MainActivity.this,R.layout.hc_c,null);
         viewList.add(view2);
+        pcv=view.findViewById( R.id.pie );
         for (int i=0;i<3;i++){
             nameList.add(names[i]);
         }
@@ -89,6 +114,42 @@ public class MainActivity extends AppCompatActivity {
                         mapList.add(map);
                     }
                     tv_sumBoxOffice.setText(realTimeBoxOffice);
+
+
+
+                    for (int i=0;i<pieData.length;i++){
+                        SliceValue sliceValue=new SliceValue( pieData[i],color[i] );
+                        sliceValues.add( sliceValue );
+                    }
+                    data=new PieChartData(  );
+                    data.setCenterCircleColor( Color.WHITE );//设置中心圆的颜色
+                    data.setHasCenterCircle( true );//设置是否有中心圆
+
+                    data.setHasLabels( true );//是否有数据显示
+                    data.setCenterCircleScale( 0.5f );//设置中心相对于全部的占比大小
+                    data.setCenterText1( "数据" );//设置center默认数据
+                    data.setHasLabelsOnlyForSelected( true );//这个表示是否选中时才显示数据，true为选中时才有数据
+                    data.setHasLabelsOutside( false );//这个表示数据是否显示在外面
+                    data.setValues(sliceValues);//这个表示为PieChartData建立数据（List类型数据）
+
+                    pcv.setPieChartData( data );
+                    pcv.setValueSelectionEnabled( true );//是否可以选择，true为可以且会变大
+                    pcv.setAlpha( 0.9f );//透明度
+                    pcv.setCircleFillRatio( 1 );//设置相对于view大小，默认为全部
+                    final PieChartOnValueSelectListener pieChartOnValueSelectListener=new PieChartOnValueSelectListener() {
+                        @Override
+                        public void onValueSelected(int i, SliceValue sliceValue) {//i表示点击的那一项，slicevalue为点击的那个的值
+                            data.setCenterText1( stateChar[i] );
+                            data.setCenterText2( sliceValue.getValue()+"("+calPercent( i )+")" );
+                        }
+
+                        @Override
+                        public void onValueDeselected() {
+
+                        }
+                    };
+                    pcv.setOnValueTouchListener( pieChartOnValueSelectListener );
+
                     MyAdapter adapter = new MyAdapter(MainActivity.this, mapList);
                     lv.setAdapter(adapter);
 
@@ -151,4 +212,14 @@ public class MainActivity extends AppCompatActivity {
         ll_content = (LinearLayout) findViewById(R.id.ll_content);
         ll_main = (LinearLayout) findViewById(R.id.ll_main);
     }
+    private String calPercent(int i){//有参数就要return,
+        String result="";
+        int sum=0;
+        for (int j=0;j<pieData.length;j++){//这里计算出pieData的全部值的和，sum
+            sum+=pieData[j];
+        }
+        result=String.format( "%.2f",(float)pieData[i]*100/sum )+"%";
+        return result;
+    }
+
 }
